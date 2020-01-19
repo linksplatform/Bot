aef004e3326e46764df37b6ac2fc4ca4d3e9c171
Mkolba <58112106+Mkolba@users.noreply.github.com>
2020-01-19 16:41:48 +0500

    Create tokens.py

---
+++ python/tokens.py
@@ -0,0 +1,9 @@
+  # -*- coding: utf-8 -*-
+
+BotToken = ''
+group_id = 0
+
+# You can get it there:
+# https://oauth.vk.com/authorize?client_id=2685278&scope=1073737727&redirect_uri=https://api.vk.com/blank.html&display=page&response_type=token&revoke=1
+
+UserToken = ''
