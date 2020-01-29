3830d89445281ab8ac912f8f07d242dddbb29ee8
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-29 23:23:26 +0700

    Update patterns.py

--- python/patterns.py
+++ python/patterns.py
@@ -3,9 +3,9 @@ from regex import compile, IGNORECASE
 from config import default_programming_languages_pattern_string as default_languages
 
 TOP = compile(r"\A\s*(топ|top)\s*\Z", IGNORECASE)
+TOP_LANGUAGES = compile(r"\A\s*(топ|top)\s*" + default_languages + r"\s*\Z", IGNORECASE)
 HELP = compile(r"\A\s*(помощь|help)\s*\Z", IGNORECASE)
 RATING = compile(r"\A\s*(рейтинг|rating)\s*\Z", IGNORECASE)
-RATING_LANGUAGES = compile(r"\A\s*(рейтинг|rating)\s*" + default_languages + r"\s*\Z", IGNORECASE)
 
 PROGRAMMING_LANGUAGES = compile(r"\A\s*\+=\s*(" + default_languages + r")\s*\Z")
 PROGRAMMING_LANGUAGES_MATCH = compile(r"\A\s*\+=\s*(?P<language>" + default_languages + r")\s*\Z")
