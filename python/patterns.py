56302cca0c02bb425ef6cdadd45ec9df99371fe0
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-29 23:19:27 +0700

    Update patterns.py

--- python/patterns.py
+++ python/patterns.py
@@ -5,6 +5,7 @@ from config import default_programming_languages_pattern_string as default_langu
 TOP = compile(r"\A\s*(топ|top)\s*\Z", IGNORECASE)
 HELP = compile(r"\A\s*(помощь|help)\s*\Z", IGNORECASE)
 RATING = compile(r"\A\s*(рейтинг|rating)\s*\Z", IGNORECASE)
+RATING_LANGUAGES = compile(r"\A\s*(рейтинг|rating)\s*" + default_languages + r"\s*\Z", IGNORECASE)
 
 PROGRAMMING_LANGUAGES = compile(r"\A\s*\+=\s*(" + default_languages + r")\s*\Z")
 PROGRAMMING_LANGUAGES_MATCH = compile(r"\A\s*\+=\s*(?P<language>" + default_languages + r")\s*\Z")
