0bca2586289f807c402feaec5b74e01f60d25ab7
Konstantin Diachenko <drakonard@gmail.com>
2020-06-02 21:26:02 +0300

    Votes deletion disabled.

--- python/config.py
+++ python/config.py
@@ -5,13 +5,13 @@ bot_group_id = 190877945
 
 # Pluses and minuses will be removed in these chats
 chats_deleting = [
-    2000000001,
+    #2000000001,
     #2000000006
 ]
 
 # Check your search line, when you`re in the needed chat. Then copy it`s id after "vk.com/im?peers=c"
 userbot_chats = {
-    2000000001: 477,
+    #2000000001: 477,
     #2000000006: 423
 }
 
