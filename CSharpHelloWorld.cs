1c6be3e14759ba9eda40d69dde6f063f932a04b8
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-04-03 16:21:00 +0300

    Add files via upload

---
+++ CSharpHelloWorld.cs
@@ -0,0 +1,54 @@
+﻿
+namespace csharp
+{
+    class CSharpHelloWorld
+    {
+       public static readonly string ProgramCs =
+ @"
+using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello World!"");
+        }
+    }
+}";
+
+       public static readonly string ProgramCsproj =
+@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+
+  <PropertyGroup>
+     <OutputType>Exe</OutputType>
+    <TargetFramework>net5.0</TargetFramework>
+   
+     </PropertyGroup>
+   
+
+</Project> ";
+
+       public static readonly string dotnetYml =
+@"name: .NET
+
+on:
+  push:
+    branches: [ main ]
+  pull_request:
+    branches: [ main ]
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v2
+    - name: Run
+      run: dotnet run"; 
+
+    }
+}
