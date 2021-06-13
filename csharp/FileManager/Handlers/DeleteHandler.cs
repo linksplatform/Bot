1e70f7d2b4092ef144794aec4fae2c09d0a7b518
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-06-13 17:30:32 +0300

    Add files via upload

---
+++ csharp/FileManager/Handlers/DeleteHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Interfaces;
+
+namespace FileManager
+{
+    public class DeleteHandler : ITrigger<Arguments>
+    {
+        public bool Condition(Arguments arguments)
+        {
+            return (arguments.Args[0].ToLower() == "delete");
+        }
+
+        public void Action(Arguments arguments)
+        {
+            if (arguments.FileStorage.LinkExist(arguments.Args[1]))
+            {
+                arguments.FileStorage.Delete(arguments.Args[1]);
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+        }
+    }
+}
