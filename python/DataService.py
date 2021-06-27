54753a73bd7b251ce8f7ea905c34c402b30b6706
heprajicfyz1 <84664570+heprajicfyz1@users.noreply.github.com>
2021-06-28 09:35:15 +1000

    Create DataService.py

---
+++ python/DataService.py
@@ -0,0 +1,12 @@
+from social_ethosa import BetterBotBase
+
+сlass BetterBotBaseDataService(BetterBotBase):
+       base = BetterBotBase("users", "dat")
+       base.addPattern("karma", 0)
+       base.addPattern("programming_languages", [])
+       base.addPattern("github_profile", "")
+       base.addPattern("supporters", [])
+       base.addPattern("opponents", [])
+       base.addPattern("last_collective_vote", 0)
+
+       self.base = base
