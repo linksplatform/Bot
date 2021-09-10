f3df75f3b18ee82f1622430e2d25f4d53315834f
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-09-10 18:50:39 +0300

    returned the commented out lines of code and corrected the file names

---
+++ csharp/Platform.Bot/Program.cs
@@ -0,0 +1,44 @@
+﻿using csharp;
+using Interfaces;
+using Octokit;
+using Platform.Exceptions;
+using Platform.IO;
+using Storage.Local;
+using Storage.Remote.GitHub;
+using System;
+using System.Collections.Generic;
+
+namespace Bot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using var cancellation = new ConsoleCancellation();
+            var argumentIndex = 0;
+            var username = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "Username", args);
+            var token = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "Token", args);
+            var appName = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "App Name", args);
+            var databaseFileName = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "Database file name", args);
+            var fileSetName = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "File set name ", args);
+            var dbContext = new FileStorage(databaseFileName);
+            Console.WriteLine($"Bot has been started. {Environment.NewLine}Press CTRL+C to close");
+            try
+            {
+                var api = new GitHubStorage(username, token, appName);
+                new ProgrammerRole(
+                    new List<ITrigger<Issue>> {
+                        new HelloWorldTrigger(api, dbContext, fileSetName),
+                        new OrganizationLastMonthActivityTrigger(api),
+                        new LastCommitActivityTrigger(api)
+                    },
+                    api
+                ).Start(cancellation.Token);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToStringWithAllInnerExceptions());
+            }
+        }
+    }
+}
