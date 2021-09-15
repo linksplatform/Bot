a74a1fc224a8ba63d8baa6ba74529a39685fd5e0
FreePhoenix <freephoenix888@gmail.com>
2021-09-15 13:31:00 +0600

    Update ProtectMainBranchTrigger.cs

--- csharp/Platform.Bot/Triggers/ProtectMainBranchTrigger.cs
+++ csharp/Platform.Bot/Triggers/ProtectMainBranchTrigger.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using Interfaces;
 using Octokit;
 using Storage.Remote.GitHub;
@@ -10,15 +13,47 @@ namespace Platform.Bot
     {
         private readonly GitHubStorage Storage;
         public ProtectMainBranchTrigger(GitHubStorage storage) => Storage = storage;
-        public bool Condition(Issue issue) => issue.Title.ToLower() == "protect default branch";
+        public bool Condition(Issue issue) => issue.Title.ToLower() == "protect default branch in all organization's repositories";
 
-        public async void Action(Issue issue)
+        public void Action(Issue issue)
         {
-            
-            // Protect master branch
-            var update = new BranchProtectionSettingsUpdate(new BranchProtectionPushRestrictionsUpdate());
-            await Storage.Client.Repository.Branch.UpdateBranchProtection(issue.Repository.Id, issue.Repository.DefaultBranch, update);
-            Storage.CloseIssue(issue);
+            var repositories = Storage.Client.Repository.GetAllForOrg(issue.Repository.Owner.Login).Result;
+            var results = UpdateRepositoriesDefaultBranchProtection(repositories);
+            StringBuilder failedRepositoriesComment = new(repositories.Count * repositories[0].Name.Length);
+            foreach (var result in results.Where(result => !result.Value))
+            {
+                failedRepositoriesComment.AppendLine($"- [ ] {result.Key}");
+            }
+            if (failedRepositoriesComment.Length != 0)
+            {
+                failedRepositoriesComment.AppendLine(
+                    "TODO: Fix default branch protection of these repositories. Failed repositories:");
+                Storage.Client.Issue.Comment.Create(issue.Repository.Id, issue.Number, failedRepositoriesComment.ToString());
+            }
+            else
+            {
+                Storage.Client.Issue.Comment.Create(issue.Repository.Id, issue.Number, "Success. All repositories default branch protection is updated.");
+                Storage.CloseIssue(issue);
+            }
+        }
+
+        public Dictionary<string, bool> UpdateRepositoriesDefaultBranchProtection(IReadOnlyList<Repository> repositories)
+        {
+            Dictionary<string, bool> result = new(repositories.Count);
+            foreach (var repository in repositories)
+            {
+                if (repository.Private)
+                {
+                    continue;
+                }
+                var update = new BranchProtectionSettingsUpdate(new BranchProtectionPushRestrictionsUpdate());
+                var request =
+                    Storage.Client.Repository.Branch.UpdateBranchProtection(repository.Id,
+                        repository.DefaultBranch, update);
+                request.Wait();
+                result.Add(repository.Name, request.IsCompletedSuccessfully);
+            }
+            return result;
         }
     }
 }
