bec711c5286c4f27449cd910776c91cd5ae08810
FreePhoenix888 <freephoenix888@gmail.com>
2021-12-22 11:37:46 +0600

    Get minimumInteractionInterval from user and use it

--- csharp/Platform.Bot/Program.cs
+++ csharp/Platform.Bot/Program.cs
@@ -6,6 +6,7 @@ using Storage.Local;
 using Storage.Remote.GitHub;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using Platform.Bot.Trackers;
 using Platform.Bot.Triggers;
 
@@ -27,22 +28,26 @@ namespace Platform.Bot
             var token = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "Token", args);
             var appName = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "App Name", args);
             var databaseFileName = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "Database file name", args);
-            var fileSetName = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "File set name ", args);
+            var fileSetName = ConsoleHelpers.GetOrReadArgument(argumentIndex++, "File set name", args);
+            var minimumInteractionIntervalStringInputInSeconds = ConsoleHelpers.GetOrReadArgument(argumentIndex, "Minimum interaction interval in seconds", args);
+            var minimumInteractionInterval = TimeSpan.FromSeconds(Int32.Parse(minimumInteractionIntervalStringInputInSeconds));
             var dbContext = new FileStorage(databaseFileName);
             Console.WriteLine($"Bot has been started. {Environment.NewLine}Press CTRL+C to close");
             try
             {
                 var api = new GitHubStorage(username, token, appName);
-                new IssueTracker(
-                    new List<ITrigger<Issue>> {
-                        new HelloWorldTrigger(api, dbContext, fileSetName),
-                        new OrganizationLastMonthActivityTrigger(api),
-                        new LastCommitActivityTrigger(api),
-                        new ProtectMainBranchTrigger(api),
-                    },
-                    api
-                ).Start(cancellation.Token);
-                new PullRequestTracker(new List<ITrigger<PullRequest>> { new MergeDependabotBumpsTrigger(api) }, api).Start(cancellation.Token);
+                while (!cancellation.Token.IsCancellationRequested)
+                {
+                    new IssueTracker(new List<ITrigger<Issue>>
+                        {
+                            new HelloWorldTrigger(api, dbContext, fileSetName),
+                            new OrganizationLastMonthActivityTrigger(api),
+                            new LastCommitActivityTrigger(api),
+                            new ProtectMainBranchTrigger(api),
+                        }, api).Start();
+                    new PullRequestTracker(new List<ITrigger<PullRequest>> { new MergeDependabotBumpsTrigger(api) }, api).Start();
+                    Thread.Sleep(minimumInteractionInterval);
+                }
             }
             catch (Exception ex)
             {
