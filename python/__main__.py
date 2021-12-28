068e88c0bf9b83e6327dc73720791170b4b19db6
Ethosa <social.ethosa@gmail.com>
2021-12-28 17:09:38 +0700

    fix top langs

--- python/__main__.py
+++ python/__main__.py
@@ -49,10 +49,11 @@ class Bot(Vk):
             (patterns.PEOPLE, self.commands.top),
             (patterns.BOTTOM,
              lambda: self.commands.top(True)),
-            (patterns.TOP_LANGUAGES, self.commands.top_langs),
-            (patterns.PEOPLE_LANGUAGES, self.commands.top_langs),
-            (patterns.BOTTOM_LANGUAGES,
+            (patterns.TOP_LANGUAGES,
              lambda: self.commands.top_langs(True)),
+            (patterns.PEOPLE_LANGUAGES,
+             lambda: self.commands.top_langs(True)),
+            (patterns.BOTTOM_LANGUAGES, self.commands.top_langs),
             (patterns.APPLY_KARMA, self.commands.apply_karma),
         )
 
