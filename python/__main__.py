1af065c9e12a9994bfcb595ada28ccb36fc1b279
Ethosa <social.ethosa@gmail.com>
2022-02-02 20:18:37 +0700

    little fix

--- python/__main__.py
+++ python/__main__.py
@@ -97,8 +97,8 @@ class Bot(Vk):
         messages = self.get_messages(event)
         selected_message = messages[0] if len(messages) == 1 else None
         selected_user = (
-            self.data.get_user(selected_message["from_id"], self)
-            if selected_message else None)
+            self.data.get_user(selected_message['from_id'], self)
+            if selected_message and selected_message['from_id'] > 0 else None)
 
         self.commands.process(
             msg, peer_id, from_id, messages, msg_id,
