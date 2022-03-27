513e85772f735ebf8c7c6559adb6315ae7fac38d
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-27 12:20:47 +0600

    Add ChangeOrganizationRepositoriesDefaultBranchTrigger

---
+++ csharp/Platform.Bot/Triggers/ChangeOrganizationRepositoriesDefaultBranchTrigger.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Text;
+using Interfaces;
+using Octokit;
+using Storage.Local;
+using Storage.Remote.GitHub;
+
+namespace Platform.Bot.Triggers;
+
+public class ChangeOrganizationRepositoriesDefaultBranchTrigger : ITrigger<Issue>
+{
+    private readonly GitHubStorage _githubStorage;
+
+    private readonly FileStorage _linksStorage;
+
+    public ChangeOrganizationRepositoriesDefaultBranchTrigger(GitHubStorage githubStorage, FileStorage linksStorage)
+    {
+        _githubStorage = githubStorage;
+        _linksStorage = linksStorage;
+    }
+    public bool Condition(Issue issue)
+    {
+        return issue.Title.Contains("Change default branch in organization repositories to");
+    }
+
+    public void Action(Issue context)
+    {
+        var newDefaultBranch = context.Title.Substring("Change default branch in organization repositories to ".Length);
+        var repositories = _githubStorage.GetAllRepositories(context.Repository.Owner.Login).Result;
+        var sb = new StringBuilder();
+        foreach (var repository in repositories)
+        {
+            if (repository.DefaultBranch == newDefaultBranch)
+            {
+                continue;
+            }
+            var oldDefaultBranchSha = _githubStorage.GetBranch(repository.Id, repository.DefaultBranch).Result.Commit.Sha;
+            _githubStorage.CreateReference(repository.Id, new NewReference($"refs/heads/{newDefaultBranch}", oldDefaultBranchSha));
+            var repositoryUpdateQuery = new RepositoryUpdate(repository.Name) { DefaultBranch = newDefaultBranch };
+            _githubStorage.Client.Repository.Edit(repository.Id, repositoryUpdateQuery).ContinueWith(task =>
+            {
+                if (task.IsCompletedSuccessfully)
+                {
+                    var message = $"The default branch of {repository.Name} has been changed to {newDefaultBranch}.";
+                    Console.WriteLine(message);
+                    sb.AppendLine(message);
+                }
+            });
+        }
+        var message = $"The default branch of organization repositories has been changed to {newDefaultBranch}.";
+        Console.WriteLine(message);
+        sb.AppendLine(message);
+        _githubStorage.CreateIssueComment(context.Repository.Id, context.Number, sb.ToString()).Wait();
+        _githubStorage.CloseIssue(context);
+    }
+}
