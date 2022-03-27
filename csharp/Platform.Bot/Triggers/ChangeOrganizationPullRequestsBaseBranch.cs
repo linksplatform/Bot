eb8957f748a1b4f6632e382c9d43fee957e5241d
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-27 13:16:51 +0600

    Add ChangeOrganizationPullRequestsBaseBranch

---
+++ csharp/Platform.Bot/Triggers/ChangeOrganizationPullRequestsBaseBranch.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using Interfaces;
+using Octokit;
+using Storage.Local;
+using Storage.Remote.GitHub;
+
+namespace Platform.Bot.Triggers;
+
+public class ChangeOrganizationPullRequestsBaseBranch : ITrigger<Issue>
+{
+    private readonly GitHubStorage _githubStorage;
+
+    private readonly FileStorage _linksStorage;
+
+    public ChangeOrganizationPullRequestsBaseBranch(GitHubStorage githubStorage, FileStorage linksStorage)
+    {
+        _githubStorage = githubStorage;
+        _linksStorage = linksStorage;
+    }
+    public bool Condition(Issue issue) => issue.Title.Contains("Change organization pull requests base branch");
+
+    public void Action(Issue issue)
+    {
+        var regexMatch = Regex.Match(issue.Title, @"Change organization pull requests base branch from (?<oldBaseBranch>\S+) to (?<newBaseBranch>\S+)");
+        if (regexMatch.Groups.Count != 3)
+        {
+            _githubStorage.CreateIssueComment(issue.Repository.Id, issue.Number, "Wrong format of the title. Must be: Change organization pull requests base branch from <oldBaseBranch> to <newBaseBranch>");
+        }
+        var oldBaseBranch = regexMatch.Groups["oldBaseBranch"].Value;
+        var newBaseBranch = regexMatch.Groups["newBaseBranch"].Value;
+        var organizationRepositories = _githubStorage.GetAllRepositories(issue.Repository.Owner.Login).Result;
+        var sb = new StringBuilder();
+        foreach (var repository in organizationRepositories)
+        {
+            var pullRequests = _githubStorage.GetPullRequests(repository.Id).Result;
+            foreach (var pullRequest in pullRequests)
+            {
+                if (pullRequest.Base.Ref != oldBaseBranch)
+                {
+                    continue;
+                }
+                var updatePullRequestResponse = _githubStorage.Client.PullRequest.Update(repository.Id, pullRequest.Number, new PullRequestUpdate { Base = newBaseBranch });
+                updatePullRequestResponse.Wait();
+                var isCompletedSuccessfully = updatePullRequestResponse.IsCompletedSuccessfully;
+                if (isCompletedSuccessfully)
+                {
+                    sb.AppendLine($"✔️ The base branch of pull request {pullRequest.HtmlUrl} is updated from {oldBaseBranch} to {newBaseBranch}.");
+                }
+                else
+                {
+                    sb.AppendLine($"❌ The base branch of pull request {pullRequest.HtmlUrl} is failed to be updated from {oldBaseBranch} to {newBaseBranch}. Reason {updatePullRequestResponse.Exception.Message}");
+                }
+            }
+        }
+        var issueComment = _githubStorage.CreateIssueComment(issue.Repository.Id, issue.Number, sb.ToString()).Result;
+        _githubStorage.CloseIssue(issue);
+        Console.WriteLine($"[{issue.Title}] {issueComment.HtmlUrl}");
+    }
+}
