75b6cfc8dc234ed1c031f6cc8164dc8f651112c8
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-07 12:50:55 +0600

    Update TraderBot

--- csharp/TraderBot/Program.cs
+++ csharp/TraderBot/Program.cs
@@ -1,7 +1,4 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.UserSecrets;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using TraderBot;
 
 [assembly: UserSecretsId("2323bae0-f4bf-4c7b-90ce-1b87d3fd76a8")]
@@ -10,15 +7,14 @@ var builder = Host.CreateDefaultBuilder(args);
 var host = builder
     .ConfigureServices((context, services) =>
     {
-        // services.AddInvestApiClient((provider, settings) => settings.AccessToken = "t.CVTYuEJAOviHK8Ex9_hGQgkjLaTqINTIGxqC5twPvBZm-dc81XKWuUxD7Nz1AYgwwhmXrX4Auh5JqScww3GEpQ");
-        if (context.Configuration.GetValue<bool>("Sync"))
-        {
-            services.AddHostedService<SyncService>();
-        }
-        else
-        {
+        // if (context.Configuration.GetValue<bool>("Sync"))
+        // {
+        //     services.AddHostedService<SyncSample>();
+        // }
+        // else
+        // {
             services.AddHostedService<AsyncService>();
-        }
+        // }
         services.AddInvestApiClient((_, settings) => context.Configuration.Bind(settings));
     })
     .Build();
