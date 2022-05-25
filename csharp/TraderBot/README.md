ca990f9338d83d52116d87d1f651e1a3cf4641b5
konard <drakonard@gmail.com>
2022-05-25 16:51:34 +0300

    "Make this bot work without restarts" milestone is complete.

--- csharp/TraderBot/README.md
+++ csharp/TraderBot/README.md
@@ -1,63 +1,52 @@
-# TraderBot | Торговый бот
+# TraderBot ([русская версия](README.ru.md))
 
 This bot is participating in the [Tinkoff Invest Robot Contest](https://github.com/Tinkoff/invest-robot-contest).
-Этот бот участвует в конкурсе [Tinkoff Invest Robot Contest](https://github.com/Tinkoff/invest-robot-contest).
 
-## Prerequisites | Предварительные требования
+## Prerequisites
 * Linux, macOS, Windows
 * [Git](https://git-scm.com/downloads)
 * [.NET SDK](https://dotnet.microsoft.com/download)
 
-## Install | Установка
+## Install
 ```
 git clone https://github.com/linksplatform/Bot
 cd Bot/csharp/TraderBot
 dotnet restore
 ```
 
-## Prepare | Подготовка
+## Prepare
 
 For the bot to work, you need to get a token. You can do this here: https://www.tinkoff.ru/invest/settings.
-Для работы бота вам необходимо получить токен. Вы можете сделать это здесь: https://www.tinkoff.ru/invest/settings.
 
 Token should be placed in `appsettings.json` file at `InvestApiSettings`/`AccessToken` key.
-Токен должен быть установлен в файле `appsettings.json` в ключе `InvestApiSettings`/`AccessToken`.
 
-## Start | Запуск
+## Start
 ```sh
 dotnet run
 ```
-To stop the bot, press Ctrl+C. | Для остановки бота нажмите Ctrl+C.
+To stop the bot, press Ctrl+C.
 
-## Description | Описание
+## Description
 
 At the moment, the bot is able to:
-* Place sell order at best or +0.01 price
-* Place buy order at best price
+* Place sell order at best price or +0.01 from buy order price;
+* Place buy order at best price.
 
-The bot does single order at a time. After placing the order, the bot stops. You can start the bot again to place another order.
+After placing the order, the bot will wait for the order to be filled. Then it will place the next order:
+* After buy order is filled, the bot will place sell order at best price or +0.01 from buy order price;
+* After sell order is filled, the bot will place buy order at best price.
 You can configure the bot to trade ETF/cash pair at `TradingSettings`/`EtfTicker` and `TradingSettings`/`CashCurrency` keys in `appsettings.json` file.
 
-В настоящее время бот имеет следующие возможности:
-* Продажа по лучшей цене или +0.01
-* Покупка по лучшей цене
-
-Бот делает одну заявку за раз. После подачи заявки бот останавливается. Вы можете запустить бота снова, чтобы подать еще одну заявку.
-Вы можете настроить торговлю ETF/наличными ботом в ключах `TradingSettings`/`EtfTicker` и `TradingSettings`/`CashCurrency` в файле `appsettings.json`.
-
-## Strategy | Стратегия
+## Strategy
 
 This bot implements a simple scalping strategy.
-It helps to execute continuous loops of buying and selling each time making 1 pt of profit per each iteration of buying and selling.
-Works best on growing market.
+It executes continuous loop of buying and selling each time making 1 pt of profit per each iteration of buying and selling.
+Works best on growing market and with least volotile asset.
 
-Этот бот реализует простую стратегию скальпинга.
-Он помогает выполнять постоянный цикл покупок и продаж каждый раз получая прибыль размером в 1 пункт за каждую итерацию покупки и продажи.
-Работает наилучшим образом на растущем рынке.
-
-## Roadmap | Дорожная карта
-- [x] Make a bot that can place buy and sell orders at best price on a single run | Сделать бота, который может подавать заявки на покупку и продажу по лучшей цене за один запуск
-- [ ] Make this bot work without restarts | Сделать этот бот работал без перезапусков
-- [ ] Add short trading mode | Добавить режим короткой торговли
-- [ ] Optimize the number of requests to the API | Оптимизировать количество запросов к API
-- [ ] Store all the data in the database (doublets links storage) | Хранить все данные в базе данных (хранилище связей-дуплетов)
+## Roadmap
+- [x] Make a bot that can place buy and sell orders at best price on a single run
+- [x] Make this bot work without restarts
+- [ ] Support moving buy order up
+- [ ] Add short trading mode
+- [ ] Optimize the number of requests to the API
+- [ ] Store all the data in the database (doublets links storage)
--- csharp/TraderBot/TradingService.cs
+++ csharp/TraderBot/TradingService.cs
@@ -1,45 +1,37 @@
 using System.Collections.Concurrent;
-using Google.Protobuf.Collections;
 using Grpc.Core;
 using Google.Protobuf.WellKnownTypes;
-using Platform.Collections;
 using Tinkoff.InvestApi;
 using Tinkoff.InvestApi.V1;
+using Platform.Collections;
 
 namespace TraderBot;
 
 public class TradingService : BackgroundService
 {
     protected readonly InvestApiClient InvestApi;
-    protected readonly IHostApplicationLifetime Lifetime;
     protected readonly ILogger<TradingService> Logger;
     protected readonly TradingSettings Settings;
     protected readonly Account CurrentAccount;
-    protected Etf CurrentInstrument;
+    protected readonly Etf CurrentInstrument;
     protected readonly decimal PriceStep;
     protected decimal CashBalance;
     protected volatile int AreOrdersActive;
-    protected ConcurrentDictionary<string, OrderState> ActiveOrders;
+    protected readonly ConcurrentDictionary<string, OrderState> ActiveOrders;
 
-    public TradingService(ILogger<TradingService> logger, InvestApiClient investApi, IHostApplicationLifetime lifetime, TradingSettings settings)
+    public TradingService(ILogger<TradingService> logger, InvestApiClient investApi, TradingSettings settings)
     {
         Logger = logger;
         InvestApi = investApi;
-        Lifetime = lifetime;
         Settings = settings;
-        
         Logger.LogInformation($"ETF ticker: {Settings.EtfTicker}");
         Logger.LogInformation($"CashCurrency: {Settings.CashCurrency}");
-
         CurrentAccount = InvestApi.Users.GetAccounts().Accounts[0];
         Logger.LogInformation($"CurrentAccount: {CurrentAccount}");
-        
         CurrentInstrument = InvestApi.Instruments.Etfs().Instruments.First(etf => etf.Ticker == Settings.EtfTicker);
         Logger.LogInformation($"CurrentInstrument: {CurrentInstrument}");
-
         PriceStep = QuotationToDecimal(CurrentInstrument.MinPriceIncrement);
         Logger.LogInformation($"PriceStep: {PriceStep}");
-
         ActiveOrders = new ConcurrentDictionary<string, OrderState>();
     }
 
@@ -103,13 +95,11 @@ public class TradingService : BackgroundService
     protected async Task PlaceNewOrders(CancellationToken cancellationToken)
     {
         Interlocked.Exchange(ref AreOrdersActive, 0);
-
         var rubBalanceMoneyValue = InvestApi.Operations
             .GetPositionsAsync(new PositionsRequest {AccountId = CurrentAccount.Id}).ResponseAsync.Result.Money
             .First(moneyValue => moneyValue.Currency == Settings.CashCurrency);
         CashBalance = MoneyValueToDecimal(rubBalanceMoneyValue);
         Logger.LogInformation($"Cash ({Settings.CashCurrency}) amount: {CashBalance}");
-
         var openOperations = GetOpenOperations();
         Logger.LogInformation($"Open operations count: {openOperations.Count}");
         if (CashBalance <= 0m && openOperations.IsNullOrEmpty())
@@ -117,9 +107,7 @@ public class TradingService : BackgroundService
             Logger.LogInformation($"No cash and assets to trade");
             return;
         }
-
         var openOperationsGroupedByPrice = openOperations.GroupBy(operation => operation.Price).ToList();
-
         var newOrdersToken = new CancellationTokenSource();
         var marketDataStream = InvestApi.MarketDataStream.MarketDataStream();
         await marketDataStream.RequestStream.WriteAsync(new MarketDataRequest
@@ -135,7 +123,6 @@ public class TradingService : BackgroundService
             {
                 throw new Exception("Error while subscribing to market data");
             }
-
             Logger.LogInformation("Subscribed to market data");
         }, cancellationToken);
         await foreach (var data in marketDataStream.ResponseStream.ReadAllAsync(newOrdersToken.Token))
@@ -144,12 +131,11 @@ public class TradingService : BackgroundService
             {
                 newOrdersToken.Cancel();
             }
-
             if (data.PayloadCase == MarketDataResponse.PayloadOneofCase.Orderbook)
             {
                 var orderBook = data.Orderbook;
                 Logger.LogInformation("Orderbook data received from stream: {OrderBook}", orderBook);
-
+                // Process potential sell order
                 if (openOperationsGroupedByPrice.Any())
                 {
                     var bestAskPrice = orderBook.Asks[0].Price;
@@ -159,16 +145,12 @@ public class TradingService : BackgroundService
                     {
                         var groupPrice = MoneyValueToDecimal(group.Key);
                         Logger.LogInformation($"groupPrice: {groupPrice}");
-
                         var targetSellPriceCandidate = groupPrice + PriceStep;
                         Logger.LogInformation($"targetSellPriceCandidate: {targetSellPriceCandidate}");
-
                         var targetSellPrice = System.Math.Max(targetSellPriceCandidate, bestAsk);
                         Logger.LogInformation($"targetSellPrice: {targetSellPrice}");
-
                         var amount = group.Sum(o => o.Trades.Sum(t => t.Quantity));
                         Logger.LogInformation($"amount: {amount}");
-
                         var isOrderPlaced = await TryPlaceSellOrder(amount, targetSellPrice);
                         if (isOrderPlaced)
                         {
@@ -176,12 +158,11 @@ public class TradingService : BackgroundService
                         }
                     }
                 }
-
+                // Process potential buy order
                 var bestBidPrice = orderBook.Bids[0].Price;
                 var bestBid = QuotationToDecimal(bestBidPrice);
                 var lotSize = CurrentInstrument.Lot;
                 var lotPrice = bestBid * lotSize;
-
                 if (CashBalance > lotPrice)
                 {
                     var lots = (long) (CashBalance / lotPrice);
@@ -191,7 +172,6 @@ public class TradingService : BackgroundService
                         Interlocked.Exchange(ref AreOrdersActive, 1);
                     }
                 }
-
                 if (AreOrdersActive == 1)
                 {
                     newOrdersToken.Cancel();
