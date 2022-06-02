f1ca5754157ec2b5a91f7f3fcf8b03e0dba8a9f9
konard <drakonard@gmail.com>
2022-06-02 22:36:23 +0300

    Warnings repair.

--- csharp/TraderBot/TradingSettings.cs
+++ csharp/TraderBot/TradingSettings.cs
@@ -2,8 +2,8 @@ namespace TraderBot;
 
 public class TradingSettings
 {
-    public string EtfTicker { get; set; }
-    public string CashCurrency { get; set; }
+    public string? EtfTicker { get; set; }
+    public string? CashCurrency { get; set; }
     public int AccountIndex { get; set; }
     public bool AllowSamePriceSell { get; set; }
 }
